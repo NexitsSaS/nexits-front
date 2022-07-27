b31ca7c122c28ae36af37ee03bcd536300bbd3ae
unknown <jakommag@gmail.com>
2022-07-27 16:50:38 +0200

    prepping for merge

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -8,16 +8,17 @@ import Lazy from 'yup/lib/Lazy';
 // dashboard routing
 const DashboardDefault = Loadable(lazy(() => import('views/dashboard/Default')));
 
+
 // utilities routing
 // const UtilsTypography = Loadable(lazy(() => import('views/utilities/Typography')));
 // const UtilsColor = Loadable(lazy(() => import('views/utilities/Color')));
 // const UtilsShadow = Loadable(lazy(() => import('views/utilities/Shadow')));
 // const UtilsMaterialIcons = Loadable(lazy(() => import('views/utilities/MaterialIcons')));
 // const UtilsTablerIcons = Loadable(lazy(() => import('views/utilities/TablerIcons')));
-const UtilsMainStats = Loadable(lazy(() => import('views/dashboard/Default/new components/mainstats')));
-const UtilsMainCatalog = Loadable(lazy(() => import('views/dashboard/Default/new components/catalog')));
-const UtilsMainPackHist = Loadable(lazy(() => import('views/dashboard/Default/new components/historypack')));
-const UtilsTeams = Loadable(lazy(() => import('views/dashboard/Default/new components/Team')));
+const UtilsMainStats = Loadable(lazy(() => import('views/dashboard/Default/new-components/mainstats')));
+const UtilsMainCatalog = Loadable(lazy(() => import('views/dashboard/Default/new-components/catalog')));
+const UtilsMainPackHist = Loadable(lazy(() => import('views/dashboard/Default/new-components/historypack')));
+const UtilsTeams = Loadable(lazy(() => import('views/dashboard/Default/new-components/Team')));
 
 // sample page routing
 // const SamplePage = Loadable(lazy(() => import('views/sample-page')));
